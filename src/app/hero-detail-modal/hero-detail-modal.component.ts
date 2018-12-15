264d7f9480439049926b2682cbf669cc76536c47
Sergii <you@example.com>
2018-12-15 17:21:57 -0500

    fix undefined hero message in the console log

--- src/app/hero-detail-modal/hero-detail-modal.component.ts
+++ src/app/hero-detail-modal/hero-detail-modal.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { Location } from '@angular/common'
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail-modal',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroDetailModalComponent implements OnInit {
 
-  constructor() { }
+  hero: Hero
+
+  constructor(
+    private heroService: HeroService,
+    private location: Location,
+    private dialogRef: MatDialogRef<HeroDetailModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data
+
+  ) { }
 
   ngOnInit() {
+    this.getHero()
+  }
+
+  getHero(): void{
+    const id = +this.data.id
+    // console.log(id)
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero)
+  }
+
+  save(): void{
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack())
+  }
+
+  goBack(): void{
+    this.location.back()
   }
 
 }
+ 
